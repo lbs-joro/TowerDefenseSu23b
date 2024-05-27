e7d5a4f82dbf7e16518a92486b64acec7a6dd3e6
lbs-joro <johan.ronnas@ga.lbs.se>
2024-05-27 13:28:41 +0200

    Lektion 4 - skjutande torn

--- Assets/Scripts/TowerShooter.cs
+++ Assets/Scripts/TowerShooter.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class TowerShooter : MonoBehaviour
 {
     List<GameObject> enemiesInRange = new List<GameObject>();
+    public GameObject bulletPrefab;
+    float cooldown;
 
+    public GameObject gunPart;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,33 @@ public class TowerShooter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        cooldown -= Time.deltaTime;
+
+        if( enemiesInRange.Count > 0)
+        {
+            Vector2 aimDirection = enemiesInRange[0].transform.position - transform.position;
+
+            //Det finns en fiende i närheten!
+            if (cooldown < 0)
+            {
+                //Skjut!
+                
+                GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+                Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+
+                bulletRigidbody.velocity = aimDirection.normalized * 10f;
+
+                cooldown = 1f;
+            }
+
+            //Rotera tornet genom att räkna ut vinkeln
+            float angle = Vector2.SignedAngle(Vector2.up, aimDirection);
+
+            gunPart.transform.rotation = Quaternion.Euler(0,0,angle);
+
+        }
+
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
